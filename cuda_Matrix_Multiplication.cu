b7918f7f5e33132d3c7a11f70aa65c471594c616
yanliang1102 <yanliang1102@gamil.com>
2016-03-08 20:37:55 -0600

    more change

--- cuda_Matrix_Multiplication.cu
+++ cuda_Matrix_Multiplication.cu
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <cuda.h>
 
-
+/*
 __global__ void MatrixMulKernel(float* Md, float* Nd,float* Pd,int Width)
 {
 	float Pvalue=0;
@@ -14,26 +14,59 @@ __global__ void MatrixMulKernel(float* Md, float* Nd,float* Pd,int Width)
 	}
 
 	Pd[threadIdx.y*Width+threadIdx.x]=Pvalue;
-}
+}*/
+
+	__global__ void MatrixMulKernel(float* M,float* N, float* Pd, int blockSize,int loopTimes)
+	{
+	__shared__ float Ms[1][1];
+	__shared__ float Ns[1][1];
+	int bx=blockIdx.x;
+	int by=blockIdx.y;
+	int tx=threadIdx.x;
+	int ty=threadIdx.y;
+
+	int Row=by*blockSize+ty;
+	int Col=bx*blockSize+tx;
+
+	float sum=0;
+	for(int m=0;m<loopTimes;m++)
+	{
+		Ms[ty][tx]=M[Row*blockSize*loopTimes+(m*blockSize+tx)];
+		Ns[ty][tx]=N[Col+(m*blockSize+ty)*blockSize*loopTimes];
+		__syncthreads();
+		for(int j=0;j<blockSize;j++)
+			sum+=Ms[ty][j]*Ns[j][tx];
+		__syncthreads();
+	}
+    Pd[Row*blockSize*loopTimes+Col]=sum;
+
+	}
 
 int main()
 {
-	int Width=2;
+
+	int matrixSize=2;//pow(2,8);
+	int blockSize=1;//pow(2,4);
+	//int noOfElement=matrixSize*matrixSize;
 /*	float* M;
 	float* N;
 	float* P;
 	M=malloc(4*sizeof(float));
 	N=malloc(4*sizeof(float));
 	P=malloc(4*sizeof(float));*/
-	float M[2],N[2],P[2];
+	float M[matrixSize],N[matrixSize],P[matrixSize];
 
-	for(int i=0;i<4;i++)
+	for(int i=0;i<matrixSize-1;i++)
 	{
 		M[i]=i+1.0;
 		N[i]=i+2.0;
 		P[i]=0.0;
 	}
 
+	float* Pd;
+    int size=matrixSize*sizeof(float);
+	cudaMalloc((void**)&Pd,size);
+/*
 	int size=Width*Width*sizeof(float);
     float* Md, *Nd, *Pd;
 
@@ -54,6 +87,19 @@ int main()
 	cudaFree(Pd);
 
 	for(int i=0;i<4;i++)
+	{
+		printf("result: %f \n",P[i]);
+	}*/
+    dim3 dimGrid(matrixSize/blockSize,matrixSize/blockSize);
+    dim3 dimBlock(blockSize,blockSize);
+    MatrixMulKernel<<<dimGrid,dimBlock>>>(M,N,Pd,blockSize,matrixSize/blockSize);
+
+    cudaMemcpy(P,Pd,size,cudaMemcpyDeviceToHost);
+
+
+	cudaFree(Pd);
+
+	for(int i=0;i<matrixSize;i++)
 	{
 		printf("result: %f \n",P[i]);
 	}
