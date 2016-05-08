3bb51091283785fb4ec26330a01d6ab34e4daad5
Yan <yan@trelawny.ou.edu>
2016-05-08 14:39:09 -0500

    first work version for H solution in 5.4.2.30

--- blockCyclicReduction/block.cu
+++ blockCyclicReduction/block.cu
@@ -1,7 +1,10 @@
      #include <stdio.h>
     #include <cuda.h>
     #include <time.h>
+    #include <math.h>
+    //#include <unistd.h>
     #define EXPO 2 //so [0,1] will be break into 2^6 intervals 64*64
+    #define PI 3.14159265
 
     __global__ void CalculateTheD(int step,float* deviceB, float* deviceC, float* deviceD, float* deviceX, float* devicenewB, float* devicenewC, float* devicenewD)
     {
@@ -126,9 +129,119 @@
         }*/
 
     }
-       
-      
+//***************************begin of unblock version of cyclic reduction*********************************************************************************//
+     __global__ void CalculatePArrayKernel(int step,int blockRow, int blockColumn,float* deviceA, float* deviceB, float* deviceC, float* deviceD)
+    {
+      int bx=blockIdx.x;
+      int by=blockIdx.y;
+      int tx=threadIdx.x;
+      int ty=threadIdx.y;
 
+      int helper11=pow(2.0,(EXPO+1)*1.0);
+      int helper22=pow(2.0,(EXPO-step+1)*1.0);
+      int helper44=pow(2.0,(EXPO-step+2)*1.0);
+      int helper33=pow(2.0,EXPO*1.0)-1;
+        //printf("step is running: %d \n",step);
+
+     // if(helper3<pow(2.0,(EXPO-step)*1.0)-1)
+        //--step 1 is special case.
+     /*  if((tx!=(blockColumn-1))&&(ty!=(blockRow-1)))-----this is very important branch divergence happen here, need
+     //to figure out how exactly cuda works!!
+        /*****calcualte A******************/
+        int helper1=helper11;
+        int helper2=helper22;
+        int helper4=helper44;
+        int flag=0;//special for step1.
+        if(step==1)
+        {
+            helper1=0;
+            helper2=0;
+            helper4=0;
+            flag=1;
+        }
+
+        int helper3=ty*blockColumn+tx+1;
+        if(helper3<=(pow(2.0,1.0*(EXPO-step))-1.0))
+        {
+        float ahelperfora1=deviceA[-step+helper1-helper4+2*(helper3)];
+        float ahelperfora2=deviceA[-step+helper1-helper4+2*(helper3)-1];
+        float bhelperfora1=deviceB[-step+helper1-helper4+2*(helper3)-1];
+        deviceA[-1-step+helper1-helper2+helper3+flag*(1+helper33)]=-1*(ahelperfora1)*ahelperfora2/bhelperfora1;
+
+        //*****calculate C******************/
+        float chelperforc1=deviceC[-step+helper1-helper4+2*(helper3)];
+        float chelperforc2=deviceC[-step+helper1-helper4+2*(helper3)+1];
+        float bhelperforc1=deviceB[-step+helper1-helper4+2*(helper3)+1];
+        deviceC[-1-step+helper1-helper2+helper3+flag*(1+helper33)]=-1*chelperforc1*chelperforc2/bhelperforc1;
+
+        //calculate B***********************************************//
+        float bhelperforb1=deviceB[-step+helper1-helper4+2*(helper3)];
+        float bhelperforb2=deviceB[-step+helper1-helper4+2*(helper3)-1];
+        float bhelperforb3=deviceB[-step+helper1-helper4+2*(helper3)+1];
+        float ahelperforb1=deviceA[-step+helper1-helper4+2*(helper3)];
+        float ahelperforb2=deviceA[-step+helper1-helper4+2*(helper3)+1];
+        float chelperforb1=deviceC[-step+helper1-helper4+2*(helper3)-1];
+        float chelperforb2=deviceC[-step+helper1-helper4+2*(helper3)];
+        deviceB[-1-step+helper1-helper2+helper3+flag*(1+helper33)]=bhelperforb1-ahelperforb1/bhelperforb2*chelperforb1-chelperforb2/bhelperforb3*ahelperforb2;
+
+        //calculate D***************************************************//
+        float dhelperford1=deviceD[-step+helper1-helper4+2*(helper3)];
+        float dhelperford2=deviceD[-step+helper1-helper4+2*(helper3)-1];
+        float dhelperford3=deviceD[-step+helper1-helper4+2*(helper3)+1];
+        float ahelperford1=deviceA[-step+helper1-helper4+2*(helper3)];
+        float bhelperford1=deviceB[-step+helper1-helper4+2*(helper3)-1];
+        float bhelperford2=deviceB[-step+helper1-helper4+2*(helper3)+1];
+        float chelperford1=deviceC[-step+helper1-helper4+2*(helper3)];
+        deviceD[-1-step+helper1-helper2+helper3+flag*(1+helper33)]=dhelperford1-ahelperford1/bhelperford1*dhelperford2-chelperford1/bhelperford2*dhelperford3;
+        for(int i=0;i<6;i++)
+        {
+        	//printf("cudab %lf \n",deviceB[i]);
+        	printf("cudab %lf \n",deviceB[i]);
+        }
+
+        for(int i=0;i<6;i++)
+        {
+        	//printf("cudab %lf \n",deviceB[i]);
+        	printf("cudad %lf \n",deviceD[i]);
+        }
+    }
+
+        __syncthreads();
+    }
+    
+
+        __global__ void BackwardKernel(int k,int blockRow, int blockColumn,float* deviceA, float* deviceB, float* deviceC, float* deviceD,float* deviceFinalX,float initialValue)
+     {
+      int bx1=blockIdx.x;
+      int by1=blockIdx.y;
+      int tx1=threadIdx.x;
+      int ty1=threadIdx.y;
+      //printf("inside of kernle %f \n",deviceFinalX[4]);
+
+      int backhelper1=ty1*blockColumn+tx1+1;
+      int backhelper2=2*backhelper1-1;//(int((2*backhelper1-1)*pow(2.0,1.0*(k-1))))/(int)(pow(2.0,(k-1)*1.0));
+      int backhelper3=(int)pow(2.0,(EXPO+1)*1.0);
+      int backhelper4=(int)pow(2.0,(EXPO-k+2)*1.0);
+
+
+      int h=(int)(pow(2.0,1.0*(k-1)));
+
+      float backhelperd=deviceD[-k+backhelper3-backhelper4+backhelper2];
+      float backhelpera=deviceA[-k+backhelper3-backhelper4+backhelper2];
+      float backhelperb=deviceB[-k+backhelper3-backhelper4+backhelper2];
+      float backhelperc=deviceC[-k+backhelper3-backhelper4+backhelper2];
+
+      int xindex1=backhelper2*pow(2.0,1.0*(k-1))-h;
+      int xindex2=backhelper2*pow(2.0,1.0*(k-1))+h;
+
+      //so thread i will be in charge of (2i-1)*2^(k-1) calculation
+      //printf("%d ",int((2*backhelper1-1)*pow(2.0,1.0*(k-1))));
+      deviceFinalX[(int)(backhelper2*pow(2.0,1.0*(k-1)))]=(backhelperd-backhelpera*deviceFinalX[xindex1]-backhelperc*deviceFinalX[xindex2])*1.0/backhelperb;
+
+      __syncthreads();
+     }
+      
+//***************************end of not block version of cyclic reduction*********************************************************************************//
     
     int main()
     {
@@ -136,6 +249,7 @@
 
       //matrix size will be 63*63 as our setup
       int m=pow(2,EXPO)-1;
+      int loopH=pow(2,EXPO-1);
          
 
       //syntax will follow the  routine in the book
@@ -240,9 +354,10 @@
       cudaMemcpy(devicenewD,newD,chunkSize,cudaMemcpyHostToDevice);
         
         //int gridSize=((m+1)/maxBlockSize)*((m+1)/maxBlockSize); //gridSize for this problem will be 16
+         	//dim3 dimGrid(1,gridSize)
       	dim3 dimGrid(1,1);  //since the maximum process we are going to use will be 63*63
-      	//int blockRow=maxBlockSize;
-      	int blockRow=maxBlockSize;//pow(2,EXPO/2); //here will be 8 and 8
+
+        int blockRow=maxBlockSize;//pow(2,EXPO/2); //here will be 8 and 8
         int blockColumn=maxBlockSize;//pow(2,EXPO/2); //here will be 8 and 8
         dim3 dimBlock(blockColumn,blockRow);
 
@@ -256,32 +371,192 @@
       }
 
       //the last step here will be 5 so it will write its new value into newB, newC, newD. 
-
       cudaMemcpy(newD,devicenewD,chunkSize,cudaMemcpyDeviceToHost);
-      for (int i=0;i<m;i++)
+    /*  for (int i=0;i<m;i++)
       {
       	if(i%8==0)
       	{
       		printf("\n");
       	}
-        printf("%lf ",newD[m+i]);
+      	printf("%lf ",newD[3+i]);
+      }*/
+      //release some of the memory
+      cudaFree(deviceB);
+      cudaFree(deviceC);
+      cudaFree(deviceD);
+      cudaFree(devicenewB);
+      cudaFree(devicenewC);
+      cudaFree(devicenewD);
+      
+      free(B);
+      free(C);
+      free(D);
+      free(newB);
+      free(newC);
+      free(newD);
+
+      cudaMemcpy(deviceB,B,chunkSize,cudaMemcpyHostToDevice);
+      cudaMemcpy(deviceC,C,chunkSize,cudaMemcpyHostToDevice);
+
+      //z will D in the not block version of cyclic reduction, ZA, ZB, ZC will corresponding to A, B and C
+      float *Z,*ZA,*ZB,*ZC,*FinalX;
+      int finalLengthX=(int)pow(2,EXPO)+1;
+      int chunkLength1=(pow(2,EXPO)-1)*2+1;
+      int zSize=chunkLength1*sizeof(float);
+      
+      Z=(float*)malloc(zSize);
+      ZA=(float*)malloc(zSize);
+      ZB=(float*)malloc(zSize);
+      ZC=(float*)malloc(zSize);
+      FinalX=(float*)malloc(finalLengthX*sizeof(float));  //the first and last one should be know by the boundary condition
+
+      float *deviceZ,*deviceZA,*deviceZB, *deviceZC,*deviceFinalX;
+	    cudaMalloc((void**)&deviceZ,zSize);
+	    cudaMalloc((void**)&deviceZA,zSize);
+	    cudaMalloc((void**)&deviceZB,zSize);
+	    cudaMalloc((void**)&deviceZC,zSize);
+	    cudaMalloc((void**)&deviceFinalX,finalLengthX*sizeof(float));
+
+
+      //set up the matrix step 
+      for(int j=1;j<=loopH;j++)
+      {
+      	//for each j, za,zb,zc all going to be different
+      	ZA[0]=0;
+
+      	for(int i=1;i<m;i++)
+      	{
+      		ZA[i]=1.0;
+      	}
+      	//else will be 0,since it has been seperate to half and half
+        for(int i=m;i<chunkLength;i++)
+        {
+          ZA[i]=0;
+        }
+
+        for(int i=0;i<m;i++)
+        {
+          ZB[i]=-4.0+2*cos((2.0*j-1.0)/(m+1.0)*PI);
+          //printf("zb:%f \n",ZB[i]);
+        }
+        for(int i=m;i<chunkLength;i++)
+        {
+          ZB[i]=0;
+        }
+
+        ZC[m-1]=0;
+        for(int i=0;i<m-1;i++)
+        {
+            ZC[i]=1.0;
+        }
+        for(int i=m;i<chunkLength;i++)
+        {
+          ZC[i]=0;
+        }
+
+        //if it is the first step z will be from d, otherwise, z will be from the previous solution of x
+        if(j==1)
+        {
+        	for(int i=0;i<m;i++)
+        	{
+        		Z[i]=newD[(loopH-1)*m+i]*(-1.0);
+        		printf("this original one being called? %lf \n",Z[i]);
+        	}
+        	 for(int i=m;i<chunkLength;i++)
+		        {
+		          Z[i]=0;
+		        }
+        }
+        else
+        {
+        	for(int i=0;i<m;i++)
+        	{
+             //to do this will be x
+        		Z[i]=FinalX[i+1];
+        		printf("does this ever called? %lf \n",Z[i]);
+        	}
+        	 for(int i=m;i<chunkLength;i++)
+		        {
+		          Z[i]=0;
+		        }
+        }
+
+        //now need to call the cyclic function to find the solution of x
+
+        cudaMemcpy(deviceZ,Z,zSize,cudaMemcpyHostToDevice);
+        cudaMemcpy(deviceZA,ZA,zSize,cudaMemcpyHostToDevice);
+        cudaMemcpy(deviceZB,ZB,zSize,cudaMemcpyHostToDevice);
+        cudaMemcpy(deviceZC,ZC,zSize,cudaMemcpyHostToDevice);
+
+        for(int j=1;j<EXPO;j++)
+        {
+        //the lock size should change, the first step it will need 2^(n-j)-1, so first step will be 3 if n=3
+        dim3 dimGrid(1,1);
+        int blockRow=pow(2,(EXPO-j)/2);
+        //printf("blockrow is :%d \n",blockRow);
+        int blockColumn=pow(2,EXPO-j-(EXPO-j)/2);
+        //printf("blockColumn is :%d \n",blockColumn);
+        dim3 dimBlock(blockColumn,blockRow);
+          //in each step the processor being used should decrease should be 2^(n-j)-1 in jth step
+        CalculatePArrayKernel<<<dimGrid,dimBlock>>>(j,blockRow,blockColumn,deviceZA,deviceZB,deviceZC,deviceZ);
+
+        }
+
+          //backward
+        //copy the device vector to host
+        //cudaMemcpy(ZA,deviceZA,chunkSize,cudaMemcpyDeviceToHost);
+       // sleep(20);
+        cudaDeviceSynchronize(); //cpu will wait until cuda finish the job, this is such important function!
+        cudaMemcpy(ZB,deviceZB,zSize,cudaMemcpyDeviceToHost);
+        for(int i=0;i<2*m;i++)
+        {
+        	printf("zbresult:%lf \n",ZB[i]);
+        }
+        //cudaMemcpy(C,deviceC,chunkSize,cudaMemcpyDeviceToHost);
+        cudaMemcpy(Z,deviceZ,zSize,cudaMemcpyDeviceToHost);
+        int lastIndex=(int)pow(2,EXPO+1)-EXPO-3;
+        float initialValue=Z[lastIndex]/ZB[lastIndex];
+        printf("initial value: %lf \n",initialValue);
+        FinalX[0]=0;
+        FinalX[(int)pow(2,EXPO-1)]=initialValue;
+        //printf("the value in the middle is: %f and this suppose to close to 0.5 when n goes big! \n",FinalX[(int)pow(2,EXPO-1)]);
+
+         cudaMemcpy(deviceFinalX,FinalX,finalLengthX*sizeof(float),cudaMemcpyHostToDevice);
+        for(int k=EXPO-1;k>=1;k--)
+        {
+          //so the most one will use 2^(n-k) variable will be covered!
+        dim3 dimGrid(1,1);
+        int blockRow=pow(2,(EXPO-k)/2);
+        int blockColumn=pow(2,EXPO-k-(EXPO-k)/2);
+        dim3  dimBlock(blockColumn,blockRow);
+        
+        BackwardKernel<<<dimGrid,dimBlock>>>(k,blockRow,blockColumn,deviceZA,deviceZB,deviceZC,deviceZ,deviceFinalX,initialValue);
+        }
+         cudaDeviceSynchronize();
+
+        cudaMemcpy(FinalX,deviceFinalX,finalLengthX*sizeof(float),cudaMemcpyDeviceToHost);
       }
 
-      //the value of D we are look at is 
+    printf("\n final result \n");
+       for (int i=0;i<finalLengthX;i++)
+      {
+      
+      	printf("%lf ",FinalX[i]);
+      }
+  }
+
+
 
       
-    /*  for(int i=0;i<chunkLength;i++)
-      {
-      	printf("%lf ",newB[i]);
-      	if(i==32*63)
-      		printf("hello! \n");
-      	if(i%63==0)
-      	{
-      		printf("\n");
-      	}
-      }*/
-      //printf("what about this time %lf:\n",D[63]);
 
-    }
+
+
+
+
+     
+     //now we begin with the second step, based on formula 5.4.2.30
+     // in order to solve this sequentially we need the non-block version of cyclic reduction that in previous homework and sinceH here is 63*63 so that we need 63 threads at most to solve this 
+      //in this way one block should be enough.
+
 
 
